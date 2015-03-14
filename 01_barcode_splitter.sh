bbd7cbb3267181e0d9c6618585b71dff6489fc3c
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 17:00:46 -0400

    rename logs and move to logs directory

--- 01_barcode_splitter.sh
+++ 01_barcode_splitter.sh
@@ -8,6 +8,9 @@
 	/local/shared/pinsky_lab/sequencing/hiseq_2014_12_19_SEQ04/clownfish-ddradseq-seq04-for-222-cycles-hau8radxx_1_read_1_passed_filter.fastq.gz \
 	/local/shared/pinsky_lab/sequencing/hiseq_2014_12_19_SEQ04/clownfish-ddradseq-seq04-for-222-cycles-hau8radxx_1_read_2_index_read_passed_filter.fastq.gz
 
+	mv nohup.out SEQ04_bcsplit_lane1.out
+	mv SEQ04_bcsplit_lane1.out ./logs/
+
 	# 	1. Move the files created from lane 1 bar code splitter into a separate 
 # 	directory so they aren't overwritten when lane 2 runs.
 
@@ -22,6 +25,9 @@
 	/local/shared/pinsky_lab/sequencing/hiseq_2014_12_19_SEQ04/clownfish-ddradseq-seq04-for-222-cycles-hau8radxx_2_read_1_passed_filter.fastq.gz \
 	/local/shared/pinsky_lab/sequencing/hiseq_2014_12_19_SEQ04/clownfish-ddradseq-seq04-for-222-cycles-hau8radxx_2_read_2_index_read_passed_filter.fastq.gz
 
+	mv nohup.out SEQ04_bcsplit_lane2.out
+	mv SEQ04_bcsplit_lane2.out ./logs/
+
 
 # USAGE: Combine the results of the two bcsplit runs into one set of reads per pool
 
@@ -32,4 +38,10 @@ cat ./lane1/PoolB-read-1.fastq.gz ./PoolB-read-1.fastq.gz > PoolB.fastq.gz
 cat ./lane1/PoolC-read-1.fastq.gz ./PoolC-read-1.fastq.gz > PoolC.fastq.gz
 
 cat ./lane1/PoolD-read-1.fastq.gz ./PoolD-read-1.fastq.gz > PoolD.fastq.gz
+
+cat ./lane1/unmatched-read-1.fastq.gz ./unmatched-read-1.fastq.gz > unmatched-read-1.fastq.gz
+
+mv unmatched-read-1.fastq.gz ./logs/
+
+
 
