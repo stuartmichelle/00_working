46dcc4a8c06bab7ca1596ae7517faae21be31df6
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 16:48:45 -0400

    Add note about alphabetical pools for index file

--- 00_workspace.sh
+++ 00_workspace.sh
@@ -2,7 +2,7 @@
 
 # USAGE: create all of the directories and info files necessary to run stacks
 # 	1. Update seq number to match current project
-	# 2. Import index and names files.
+	# 2. Import index - make sure index is listed as Pools A, B, C, D and names files.
 
 mkdir ~/01_stacks/04seq
 mkdir ~/01_stacks/04seq/lane1 PoolA PoolB PoolC PoolD samples stacks
