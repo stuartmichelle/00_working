01d03539f43360a537fa63ec88ee3cbf929f3976
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 17:00:32 -0400

    create logs directory

--- 00_workspace.sh
+++ 00_workspace.sh
@@ -5,6 +5,6 @@
 	# 2. Import index - make sure index is listed as Pools A, B, C, D and names files.
 
 mkdir ~/01_stacks/04seq
-mkdir ~/01_stacks/04seq/lane1 PoolA PoolB PoolC PoolD samples stacks
+mkdir ~/01_stacks/04seq/lane1 PoolA PoolB PoolC PoolD samples stacks logs
 cp ~/01_stacks/barcodes.txt ~/01_stacks/04seq/
 cp ~/01_stacks/batch_1* ~/01_stacks/04seq/stacks/
