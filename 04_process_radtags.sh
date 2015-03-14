b69d3a4d2e4819aad3bb6791d8fd430ae9eb1f9b
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 17:24:48 -0400

    Clean up files at end of run

--- 04_process_radtags.sh
+++ 04_process_radtags.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 # USAGE: run process radtags on samples by pool
-    # 1. Change Pool A to Pool B and so on for each run.
+    # 1. Find seq04 and replace with current seq number
 
 process_radtags -b barcodes.txt -c -q -r \
 --renz_1 pstI --renz_2 mluCI -i gzfastq \
 --adapter_1 ACACTCTTTCCCTACACGACGCTCTTCCGATCT \
--f ./bcsplit/PoolA.fastq.gz -o ./PoolA
+-f ./PoolA.fastq.gz -o ./PoolA
 
+process_radtags -b barcodes.txt -c -q -r \
+--renz_1 pstI --renz_2 mluCI -i gzfastq \
+--adapter_1 ACACTCTTTCCCTACACGACGCTCTTCCGATCT \
+-f ./PoolB.fastq.gz -o ./PoolB
+
+process_radtags -b barcodes.txt -c -q -r \
+--renz_1 pstI --renz_2 mluCI -i gzfastq \
+--adapter_1 ACACTCTTTCCCTACACGACGCTCTTCCGATCT \
+-f ./PoolC.fastq.gz -o ./PoolC
+
+process_radtags -b barcodes.txt -c -q -r \
+--renz_1 pstI --renz_2 mluCI -i gzfastq \
+--adapter_1 ACACTCTTTCCCTACACGACGCTCTTCCGATCT \
+-f ./PoolD.fastq.gz -o ./PoolD
+
+mv nohup.out seq04_procradtags.out
+mv seq04_procradtags.out ./logs/
+
+rm ./*.fastq.gz
 
 
