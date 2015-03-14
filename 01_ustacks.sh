56e7c25e9ee73ffc933ef90922c6c94593878d6d
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 14:34:58 -0400

    Added todo items and USAGE, moved lines around

--- 01_ustacks.sh
+++ 01_ustacks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#Todo:
-#get script to prompt for which seqID we are working on and then populate the \
-# id number, samples directory and stacks directory
+#USAGE
+#	1. Change the i number to match the seq number
+#		(example: for seq04, i=40001, for seq05, i=50001)
+#	2. Change the directory to match the seq - everywhere it says seq04 below, 
+#		change to current working seq number.
 
-#Run ustacks on fastq files, 10 threads, depth of 2 to create a stack (default $
+#Run ustacks
 
 i=40001
 for file in ../04seq/samples/*.fq
@@ -15,3 +17,8 @@ do
 done
 
 
+#Todo:
+#get script to prompt for which seqID we are working on and then populate the \
+# id number, samples directory and stacks directory
+
+#add ustacks options to this file, and things I've learned about ustacks along the way
