6d2b36dd9534e660ca1d6879659e1d239f936f8b
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 16:52:37 -0400

    Add copy barcodes file from master

--- 00_workspace.sh
+++ 00_workspace.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # USAGE: create all of the directories and info files necessary to run stacks
-# 	1. Update seq number to match current project
+# 	1. Find and replace 04seq to seq number of current project
 	# 2. Import index - make sure index is listed as Pools A, B, C, D and names files.
 
 mkdir ~/01_stacks/04seq
 mkdir ~/01_stacks/04seq/lane1 PoolA PoolB PoolC PoolD samples stacks
-
+cp ~/01_stacks/barcodes.txt ~/01_stacks/04seq/barcodes.txt
