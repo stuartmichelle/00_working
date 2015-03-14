5b17a2ef86d6a5cb15f50777a5f00a0cbb7d4890
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 10:29:27 -0400

    Added files to 00_working

---
+++ 01_ustacks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#copy and paste from the google doc ligationID column to create this files list
+
+files="
+"
+
+#Run ustacks on fastq files, 10 threads, depth of 2 to create a stack (default $
+
+i=5001
+for file in $files
+do
+    ustacks -t fastq -p 10 -m 2 -i $i -f ./samples/${file}.fq \
+    -o ./stacks
+    let i+=1;
+done
