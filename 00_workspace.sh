54bfcad3325d103aaeb1820e82877ebd593d5449
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 16:40:24 -0400

    created workspace

---
+++ 00_workspace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# USAGE: create all of the directories and info files necessary to run stacks
+# 	1. Update seq number to match current project
+	# 2. Import index and names files.
+
+mkdir ~/01_stacks/04seq
+mkdir ~/01_stacks/04seq/lane1 PoolA PoolB PoolC PoolD samples stacks
+
--- 03_populations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Calculate population statistics and export several output files.
-
-populations -b 1 -P ../04seq/stacks/ -s -t 15
-
-
