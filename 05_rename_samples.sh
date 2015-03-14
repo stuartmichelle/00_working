12ce37133475409b9068af5ab6a5083b395bae6a
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 17:31:50 -0400

    file cleanup at the end of script

--- 05_rename_samples.sh
+++ 05_rename_samples.sh
@@ -24,3 +24,23 @@ mv ./PoolB/APCL* ./samples/
 mv ./PoolC/APCL* ./samples/
 mv ./PoolD/APCL* ./samples/
 
+mv ./PoolA/names ./logs/
+mv ./PoolB/names ./logs/
+mv ./PoolC/names ./logs/
+mv ./PoolD/names ./logs/
+
+mv ./PoolA/process_radtags.log ./PoolA/Aprocess_radtags.log
+mv ./PoolB/process_radtags.log ./PoolB/Bprocess_radtags.log
+mv ./PoolC/process_radtags.log ./PoolC/Cprocess_radtags.log
+mv ./PoolD/process_radtags.log ./PoolD/Dprocess_radtags.log
+
+mv ./PoolA/Aprocess_radtags.log ./logs/
+mv ./PoolB/Bprocess_radtags.log ./logs/
+mv ./PoolC/Cprocess_radtags.log ./logs/
+mv ./PoolD/Dprocess_radtags.log ./logs/
+
+rmdir PoolA
+rmdir PoolB
+rmdir PoolC
+rmdir PoolD
+
