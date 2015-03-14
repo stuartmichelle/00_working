5472a59c2c4bc06ce52c31be9764edd26d796529
Michelle Stuart <bunx21@yahoo.com>
2015-03-14 12:43:57 -0400

    Change file feed line to be automatic instead of from list

--- 02_sstacks.sh
+++ 02_sstacks.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-#copy and paste from the google doc ligationID column to create this files list
+#Todo:
+#get script to prompt for which seqID we are working on and then populate the \
+# id number, samples directory and stacks directory
 
-files="
-"
+#copy and paste from the google doc ligationID column to create this files list
 
 #Run sstacks
 
-for file in $files
+for file in $(ls -1 ../04seq/stacks/*.tags.tsv.gz \
+| grep -v catalog | perl -pe 's/\.tags\.tsv\.gz//')
 do
-    sstacks -p 10 -b 1 -c ./stacks/batch_1 \
-            -s ./stacks/$file \
+    sstacks -p 10 -b 1 -c ../04seq/stacks/batch_1 \
+            -s ../04seq/stacks/$file \
             -o ./stacks/
 done
