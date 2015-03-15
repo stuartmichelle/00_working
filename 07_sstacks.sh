6c5c9fda434ba5179337886e32ef6bff734ffdec
Michelle Stuart <bunx21@yahoo.com>
2015-03-15 08:35:22 -0400

    remove .gz from sample search

--- 07_sstacks.sh
+++ 07_sstacks.sh
@@ -8,8 +8,8 @@
 
 #Run sstacks
 
-for file in $(ls -1 ../04seq/stacks/*.tags.tsv.gz \
-| grep -v catalog | perl -pe 's/\.tags\.tsv\.gz//')
+for file in $(ls -1 ../04seq/stacks/*.tags.tsv \
+| grep -v catalog | perl -pe 's/\.tags\.tsv//')
 do
     sstacks -p 10 -b 1 -c ../04seq/stacks/batch_1 \
             -s ../04seq/stacks/$file \
