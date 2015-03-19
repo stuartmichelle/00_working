d63124dce2fe8bb458a876007956934c32f716c1
Michelle Stuart <Michelle@nrb176.deenr.rutgers.edu>
2015-03-19 16:15:48 -0400

    prepped for seq03 - removed non-needed directories

--- 00_workspace.sh
+++ 00_workspace.sh
@@ -4,10 +4,8 @@
 # 	1. Find and replace 04seq to seq number of current project
 	# 2. Import index - make sure index is listed as Pools A, B, C, D and names files.
 
-mkdir ~/01_stacks/04seq
-mkdir ~/01_stacks/04seq/lane1 PoolA PoolB PoolC PoolD samples stacks logs
-cp ~/01_stacks/barcodes.txt ~/01_stacks/04seq/
-cp ~/01_stacks/batch_1* ~/01_stacks/04seq/stacks/
+
+mkdir samples stacks logs
 
 #Aren't making database because we are combining all samples into one: aclarkii 
 #Apply stacks config to mysql database
